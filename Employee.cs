62591946954d1e00cfca922bff2fd9299485d4df
DataKakhadze <datakakhadze@gmail.com>
2024-05-27 23:30:58 +0400

    initial

---
+++ Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Employee
+    {
+        string FirstName { get; set; }
+        int Age { get; set; }
+        string Lastname { get; set; }
+        int Id { get; set; }
+        public void Age_setter(int age)
+        {
+            this.Age = age;
+        }
+        public int Age_getter()
+        {
+            return Age;
+        }
+
+        public void firstname_setter(string name)
+        {
+            this.FirstName = name;
+        }
+        public string firstname_getter()
+        {
+            return FirstName;
+        }
+        public void lastname_setter(string lastname)
+        {
+            this.Lastname = lastname;
+        }
+        public string lastname_getter()
+        {
+            return Lastname;
+        }
+        public void Id_setter(int id)
+        {
+            this.Id = id;
+        }
+        public int Id_getter()
+        {
+            return Id;
+        }
+
+
+        public Employee() {
+          
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void enter_Info()
+            {
+
+                Console.WriteLine("Enter id:");
+                string IdString = Console.ReadLine();
+
+                Console.WriteLine("Enter firstname:");
+                string firstname = Console.ReadLine();
+
+                while (firstname == "" || firstname == "-")
+                {
+                    Console.WriteLine("firstname cant be null ");
+                    firstname = Console.ReadLine();
+                }
+
+                Console.WriteLine("Enter lastname:");
+                string Lastname = Console.ReadLine();
+
+                Console.WriteLine("Enter age:");
+                string ageString = Console.ReadLine();
+
+
+                while (ageString == "" || ageString == "-")
+                {
+                    Console.WriteLine("age cant be null ");
+                    ageString = Console.ReadLine();
+                }
+
+                if (int.TryParse(IdString, out int id))
+                {
+                    Employee obj = new Employee();
+                    obj.Id_setter(id);
+                    obj.lastname_setter(Lastname);
+                }
+
+                if (int.TryParse(ageString, out int age))
+                {
+                    Employee obj = new Employee();
+                    obj.firstname_setter(firstname);
+                    obj.Age_setter(age);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid age entered. Please enter a valid integer.");
+                }
+                Console.WriteLine($"your id is {id} ,ur name is {firstname}, ur lastname is {Lastname}, ur age is {age}");
+            }
+
+
+            enter_Info();
+        }
+    }
+}
